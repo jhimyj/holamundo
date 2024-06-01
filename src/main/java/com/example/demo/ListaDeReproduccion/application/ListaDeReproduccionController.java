4e95fb6c69e87192f27d9fbbbea452f78f72f5f0
Nahia Escalante <nahiaescalanteccoyllo@gmail.com>
2024-06-01 15:47:05 -0500

    a

--- src/main/java/com/example/demo/ListaDeReproduccion/application/ListaDeReproduccionController.java
+++ src/main/java/com/example/demo/ListaDeReproduccion/application/ListaDeReproduccionController.java
@@ -1,7 +1,12 @@
 package com.example.demo.ListaDeReproduccion.application;
 
 import com.example.demo.ListaDeReproduccion.domain.ListaDeReproduccionService;
+import com.example.demo.auth.dto.JwtAuthResponse;
+import com.example.demo.auth.dto.LoginReq;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ListaDeReproduccionController {
     @Autowired
     private ListaDeReproduccionService listaDeReproduccionService;
+
+    @Autowired
+    private
+
+    @PostMapping("/")
+    public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginReq req) {
+        return ResponseEntity.ok(listaDeReproduccionService.login(req));
+    }
+
 
     //GET /users/{user_id}/playlists
     //GET /playlists/{playlist_id}
--- src/main/java/com/example/demo/ListaDeReproduccion/domain/ListaDeReproduccionService.java
+++ src/main/java/com/example/demo/ListaDeReproduccion/domain/ListaDeReproduccionService.java
@@ -8,4 +8,7 @@ import org.springframework.stereotype.Service;
 public class ListaDeReproduccionService {
     @Autowired
     private ListaDeReproduccionRepository listaDeReproduccionRepository;
+
+
+
 }
